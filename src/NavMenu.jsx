a5e20b9fc8fc5522e83ee879414fe9bdd0bcbbdc
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-20 15:37:59 -0400

    basic layout and styles for nav component done

--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -1,10 +1,34 @@
+import './NavMenu.css'
+import Logo from './assets/homeAssets/logo.png'
+import Scribble from './assets/homeAssets/scribble.png'
+
 const NavMenu = () => {
     return (
         <nav>
-            <div className="wrapper">
-                <div className="logo">
-                    <img src="" alt="" />
-                    <h2>jobi</h2>
+            <div className="navFlex wrapper">
+                <div className="navLeft">
+                    <div className="logo">
+                        <img src={Logo} alt="" />
+                        <h2>jobi</h2>
+                    </div>
+                    <button>Category</button>
+                    <ul className="navLinks">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Explore</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Pages</a></li>
+                    </ul>
+                </div>
+                <div className="scribble">
+                    <img src={Scribble} alt="" />
+                </div>
+                <div className="navRight">
+                    <ul>
+                        <li><a href="#">Post Job</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Hire Top Talents</a></li>
+                    </ul>
                 </div>
             </div>
         </nav>
