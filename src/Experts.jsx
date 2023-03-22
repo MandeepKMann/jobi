260be2988ea03049930cde337dc15fe5024c643e
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-21 20:24:43 -0400

    base styling done for experts section

--- src/Experts.jsx
+++ src/Experts.jsx
@@ -1,8 +1,29 @@
+import './Experts.scss';
+import Block1 from './assets/homeAssets/block1.jpg';
+import Block2 from './assets/homeAssets/block2.jpg';
+import Block3 from './assets/homeAssets/block3.jpg';
+import Dots from './assets/homeAssets/dots.png';
+
 const Experts = () => {
     return (
-        <section className="talentedExperts">
+        <section className="talentedExperts wrapper">
             <div className="expertsCollage">
-
+                <div className="collageTop">
+                    <div className="blockContainer1">
+                        <img src={Block1} alt="" />
+                    </div>
+                    <div className="dotsContainer">
+                        <img src={Dots} alt="" />
+                    </div>
+                </div>
+                <div className="collageBottom">
+                    <div className="blockContainer2">
+                        <img src={Block2} alt="" />
+                    </div>
+                    <div className="blockContainer3">
+                        <img src={Block3} alt="" />
+                    </div>
+                </div>
             </div>
             <div className="expertsText">
                 <h2>Get over 50,000+ talented experts in jobi.</h2>
